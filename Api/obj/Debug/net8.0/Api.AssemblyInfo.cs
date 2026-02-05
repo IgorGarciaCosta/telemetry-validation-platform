054eef7034b8daefce3ccc8719536125b3bdeaf9
IgorGarciaCosta <igonildo7@gmail.com>
2026-02-05 19:24:22 -0300

    Added postman json file

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58387f4d737523252d4dff47322483af966e8a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0759c41e2620068340f20c500ca6bb22aaba8245")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
