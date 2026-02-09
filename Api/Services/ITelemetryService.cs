19711e39e1a1c713e014317785b83751c28eb3b8
IgorGarciaCosta <igonildo7@gmail.com>
2026-02-09 01:22:03 -0300

    Added services folder and and interface to telemetry service

---
+++ Api/Services/ITelemetryService.cs
@@ -0,0 +1,10 @@
+using Api.Models;
+
+namespace Api.Services;
+
+public interface ITelemetryService
+{
+    Task<TelemetryEvent> CreateEventAsync(string type, string payload);
+    Task<TelemetryEvent> GetAllEventsAsync(string? type, DateTimeOffset? from, DateTimeOffset? to);
+    Task<TelemetryEvent?> GetEventByIdAsync(Guid id);
+}
